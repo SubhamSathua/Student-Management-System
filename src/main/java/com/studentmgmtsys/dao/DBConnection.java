ee787e6c5839a325b0ede2cd1d5e5dd988eeb6b8
Subham Kumar Sathua <137478528+SubhamSathua@users.noreply.github.com>
2025-10-13 01:14:23 +0530

    configure Env.java for local DB password

--- src/main/java/com/studentmgmtsys/dao/DBConnection.java
+++ src/main/java/com/studentmgmtsys/dao/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/studentManagementSystem";
     private static final String USER = "root";
-    private static final String PASSWORD = "1425360";
+    private static final String PASSWORD = Env.DB_PASSWORD;
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
